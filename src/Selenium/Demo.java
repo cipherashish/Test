d49f167524fed3c2d5328e0ab1d1b656b758e632
cipherashish <50736885+cipherashish@users.noreply.github.com>
2019-09-05 15:46:13 +0530

    Update Demo.java

--- src/Selenium/Demo.java
+++ src/Selenium/Demo.java
@@ -5,21 +5,33 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Demo {
 
-	public static void main(String[] args) {
-		System.setProperty("webdriver.chrome.driver","\\var\\lib\\jenkins\\workspace\\driver\\chromedriver.exe"); // <– Change this path
-		WebDriver driver = new ChromeDriver();
-		String baseUrl = "https://experitest.com/free-trial/";
-		String expectedTitle = "Free trial";
-		String actualTitle = "";
-		driver.get(baseUrl);
-		actualTitle = driver.getTitle();
-		if (actualTitle.contentEquals(expectedTitle)){
-		System.out.println("TEST PASSED!");
-		} else {
-		System.out.println("TEST FAILED");
-		}
-		driver.close();
+	public static void main(String[] args) throws InterruptedException {
+		
+	System.setProperty("webdriver.chrome.driver", "\\var\\lib\\jenkins\\workspace\\driver\\chromedriver.exe");
+  WebDriver driver =new ChromeDriver();
+  driver.get("http://www.gmail.com/");
+  driver.manage().window().maximize();
+  driver.findElement(By.id("identifierId")).sendKeys("@gmail.com");
+  Thread.sleep(2000);
+  driver.findElement(By.className("CwaK9")).click();
+  Thread.sleep(2000);
+String at =driver.getTitle();
+String et ="no";//expected title
+driver.close();
+
+
+//establish test condition
+
+if(at.equalsIgnoreCase(et))
+{
+	System.out.println("test successful");
+}
+
+else {
+	System.out.println("test failure");
+}
 	}
+
 		}
 
 		
