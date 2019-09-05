12c921854f222cbe9ccb243b60cd5daf603a9c32
cipherashish <50736885+cipherashish@users.noreply.github.com>
2019-09-05 15:10:07 +0530

    Update Demo.java

--- src/Selenium/Demo.java
+++ src/Selenium/Demo.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Demo {
 
 	public static void main(String[] args) {
-		System.setProperty("webdriver.chrome.driver","E:\\avan\\chromedriver_win32\\chromedriver.exe"); // <– Change this path
+		System.setProperty("webdriver.chrome.driver","\\var\\lib\\jenkins\\workspace\\driver\\chromedriver.exe"); // <â€“ Change this path
 		WebDriver driver = new ChromeDriver();
 		String baseUrl = "https://experitest.com/free-trial/";
 		String expectedTitle = "Free trial";
