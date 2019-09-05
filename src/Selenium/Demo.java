4342c613380ca4fcda2c72e0860bd8027787cbe9
cipherashish <ashish.090singh@gmail.com>
2019-09-05 13:08:55 +0530

    pushing files

---
+++ src/Selenium/Demo.java
@@ -0,0 +1,25 @@
+package Selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver","E:\\avan\\chromedriver_win32\\chromedriver.exe"); // <– Change this path
+		WebDriver driver = new ChromeDriver();
+		String baseUrl = "https://experitest.com/free-trial/";
+		String expectedTitle = "Free trial";
+		String actualTitle = "";
+		driver.get(baseUrl);
+		actualTitle = driver.getTitle();
+		if (actualTitle.contentEquals(expectedTitle)){
+		System.out.println("TEST PASSED!");
+		} else {
+		System.out.println("TEST FAILED");
+		}
+		driver.close();
+	}
+		}
+
+		
